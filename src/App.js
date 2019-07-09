4d13b0ba661b839c37b52d46c83184fbfcce477b
XinmengLyu <lvxinmeng_1995@163.com>
2019-07-09 18:03:26 -0400

    category

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      category: {
+        isLoading: false,
+        data: [],
+        error: null
+      },
+      detail: {
+        isLoading: false,
+        data: [],
+        error: null,
+      }
+    };
+  }
+  componentDidMount() {
+    this.setState({
+      category: {
+        isLoading: true
+      }
+    });
+    axios.get("http://stream-restaurant-menu-svc.herokuapp.com/category")
+      .then(res => {
+        this.setState({
+          category: {
+            isLoading: false,
+            data: res.data,
+            error: null
+          }
+        });
+      })
+      .catch(error => {
+        this.setState({
+          category: {
+            isLoading: false,
+            error: error,
+          }
+        });
+      });
+  }
+
+  handleClick = () => {};
+
+  render() {
+    const {category: {data, isLoading}} = this.state;
+    return (
+      <div className="App">
+        <h1>Menu Categories</h1>
+        <div className="category">
+          {isLoading? <h2>Loading...</h2> : <ul>
+            {data.map((c, i) => {
+              return <li key={i} onClick={this.handleClick}>{c.name}</li>
+            })}
+          </ul>}
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
