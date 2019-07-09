b6c48260613a59c47ad175cd50fa9d321139e650
XinmengLyu <lvxinmeng_1995@163.com>
2019-07-09 18:23:53 -0400

    items

--- src/App.js
+++ src/App.js
@@ -22,7 +22,9 @@ class App extends React.Component {
   componentDidMount() {
     this.setState({
       category: {
-        isLoading: true
+        isLoading: true,
+        data: [],
+        error: null
       }
     });
     axios.get("http://stream-restaurant-menu-svc.herokuapp.com/category")
@@ -45,19 +47,71 @@ class App extends React.Component {
       });
   }
 
-  handleClick = () => {};
+  handleClick = (sn) => {
+    this.setState({
+      detail: {
+        isLoading: true,
+        data: [],
+        error: null,
+      }
+    });
+    axios.get(`https://stream-restaurant-menu-svc.herokuapp.com/item?category=${sn}`)
+      .then(res => {
+        // console.log(res);
+        this.setState(
+          {
+            detail: {
+              isLoading: false,
+              data: res.data,
+              error: null
+            }
+          }
+        );
+      })
+      .catch(error => {
+        this.setState(
+          {
+            detail: {
+              isLoading: false,
+              error: error
+            }
+          }
+        );
+      });
+  };
 
   render() {
-    const {category: {data, isLoading}} = this.state;
+    const { category: { data, isLoading }, detail } = this.state;
+    console.log(detail);
     return (
       <div className="App">
         <h1>Menu Categories</h1>
         <div className="category">
-          {isLoading? <h2>Loading...</h2> : <ul>
+          {isLoading ? <h2>Loading...</h2> : <ul>
             {data.map((c, i) => {
-              return <li key={i} onClick={this.handleClick}>{c.name}</li>
+              return <li key={i} onClick={() => this.handleClick(c.short_name)}>{`${c.name} - (${c.short_name})`}</li>
             })}
           </ul>}
+        </div>
+        <div className="items">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Description</th>
+              </tr>
+            </thead>
+            <tbody>
+              {detail.data.map((d, i) => {
+                return (
+                  <tr key={i}>
+                    <td>{d.name}</td>
+                    <td>{d.description}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
         </div>
       </div>
     );
